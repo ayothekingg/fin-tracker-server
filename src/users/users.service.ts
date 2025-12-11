4d21a1c939d9a7cfd2b655c51be1a9d82c64c44b
ayothekingg <ayojames444@gmail.com>
2025-12-11 11:03:06 +0100

    (feat): user authentication mapped to mongodb

---
+++ src/users/users.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import { User, UserDocument } from './schemas/user.schema';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async create(
+    fullName: string,
+    email: string,
+    password: string,
+  ): Promise<User> {
+    const hashed = await bcrypt.hash(password, 10);
+
+    const user = new this.userModel({ fullName, email, password: hashed });
+    return user.save();
+  }
+
+  async findByEmail(email: string): Promise<User | undefined> {
+    const user = await this.userModel.findOne({ email }).exec();
+    return user ?? undefined;
+  }
+}
