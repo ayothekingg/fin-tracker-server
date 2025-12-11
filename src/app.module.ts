aeb6a27d4f9a3bb2245acb3ae00a17ff6ec55e41
ayothekingg <ayojames444@gmail.com>
2025-12-11 11:02:11 +0100

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { JwtModule } from './jwt/jwt.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        // 👇 ADD THIS LINE HERE
+        console.log('MONGO_URI:', configService.get('MONGO_URI'));
+
+        return {
+          uri: configService.get<string>('MONGO_URI'),
+        };
+      },
+    }),
+
+    UsersModule,
+    AuthModule,
+    JwtModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
