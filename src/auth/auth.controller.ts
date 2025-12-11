07737ed52eaa37b87ca440697e776c83a17c9d13
ayothekingg <ayojames444@gmail.com>
2025-12-11 11:02:29 +0100

    (feat): authentication logic

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+  ) {}
+
+  @Post('register')
+  async register(@Body() body: any) {
+    return this.usersService.create(body.fullName, body.email, body.password);
+  }
+
+  @Post('login')
+  async login(@Body() body: any) {
+    const user = await this.authService.validateUser(body.email, body.password);
+    return this.authService.login(user);
+  }
+}
