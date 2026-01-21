92f517ed1b7ce933c300dcf0f184e10881cc63de
ayothekingg <ayojames444@gmail.com>
2026-01-21 19:09:09 +0100

    (chore): dependency update

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { JwtModule } from './jwt/jwt.module';
-<<<<<<< HEAD
 import { ExpenseModule } from './expense/expense.module';
-=======
->>>>>>> 4d21a1c939d9a7cfd2b655c51be1a9d82c64c44b
 
 @Module({
   imports: [
@@ -21,9 +18,6 @@ import { ExpenseModule } from './expense/expense.module';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
-        // 👇 ADD THIS LINE HERE
-        console.log('MONGO_URI:', configService.get('MONGO_URI'));
-
         return {
           uri: configService.get<string>('MONGO_URI'),
         };
@@ -33,10 +27,7 @@ import { ExpenseModule } from './expense/expense.module';
     UsersModule,
     AuthModule,
     JwtModule,
-<<<<<<< HEAD
     ExpenseModule,
-=======
->>>>>>> 4d21a1c939d9a7cfd2b655c51be1a9d82c64c44b
   ],
   controllers: [AppController],
   providers: [AppService],
